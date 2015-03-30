677cd7c1501b4adb161d823498021165991423e2
ntlam <ntlam94@gmail.com>
2015-03-31 00:03:17 +0700

    Change ManageWifi layout

--- app/src/main/java/com/uet/beman/activity/BM_ActivityManageWifi.java
+++ app/src/main/java/com/uet/beman/activity/BM_ActivityManageWifi.java
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.uet.beman.R;
@@ -17,33 +18,46 @@ import com.uet.beman.util.Constant;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class BM_ActivityManageWifi extends ActionBarActivity {
 
     private Context ctx = this;
-    private ArrayList<String> configuredWifi;
     private String currentKey = null;
+
+    private EditText editTextHomeWifi;
+    private EditText editTextWorkWifi;
+    private EditText editTextGirlWifi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bm_activity_manage_wifi);
 
-        configuredWifi = getConfiguredWifi();
+        editTextHomeWifi = (EditText) findViewById(R.id.editText_home_wifi);
+        editTextWorkWifi = (EditText) findViewById(R.id.editText_work_wifi);
+        editTextGirlWifi = (EditText) findViewById(R.id.editText_girl_wifi);
+
+        loadEditText();
     }
 
-    private ArrayList<String> getConfiguredWifi() {
-        ArrayList<String> res = new ArrayList<>();
-        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        if (wifiManager != null) {
-            List<WifiConfiguration> wifiConfig = wifiManager.getConfiguredNetworks();
-            if (wifiConfig != null && !wifiConfig.isEmpty())
-                for (WifiConfiguration wc : wifiConfig)
-                    res.add(wc.SSID);
+    private void loadEditText() {
+        showWifiPreview(editTextHomeWifi, getSavedWifi(Constant.HOME_WIFI_KEY));
+        showWifiPreview(editTextWorkWifi, getSavedWifi(Constant.WORK_WIFI_KEY));
+        showWifiPreview(editTextGirlWifi, getSavedWifi(Constant.GIRL_HOUSE_WIFI_KEY));
+    }
+
+    private void showWifiPreview(EditText editText, ArrayList<String> wifiList) {
+        if (!wifiList.isEmpty()) {
+            String text = "";
+            for (String s : wifiList)
+                text += s + " , ";
+            text = text.substring(0, text.length() - 3);
+            editText.setText(text);
+        } else {
+            editText.setText("");
         }
-        Collections.sort(res);
-        return res;
     }
 
     @Override
@@ -81,11 +95,22 @@ public class BM_ActivityManageWifi extends ActionBarActivity {
     }
 
     private void showWifiDialog(String key) {
+        if (!checkEnableWifi()) {
+            Toast.makeText(ctx, "Please enable Wifi", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         currentKey = key;
         ArrayList<String> savedWifi = getSavedWifi(key);
+        ArrayList<String> configuredWifi = getConfiguredWifi();
 
         ManageWifiDialogFragment frag = ManageWifiDialogFragment.getInstance(configuredWifi, savedWifi);
         frag.show(getSupportFragmentManager(), "wifi dialog");
+    }
+
+    private boolean checkEnableWifi() {
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        return wifiManager.isWifiEnabled();
     }
 
     private ArrayList<String> getSavedWifi(String key) {
@@ -93,11 +118,38 @@ public class BM_ActivityManageWifi extends ActionBarActivity {
         return spHelper.getWifiList(key);
     }
 
+    private ArrayList<String> getConfiguredWifi() {
+        ArrayList<String> res = new ArrayList<>();
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager != null) {
+            List<WifiConfiguration> wifiConfig = wifiManager.getConfiguredNetworks();
+            if (wifiConfig != null && !wifiConfig.isEmpty())
+                for (WifiConfiguration wc : wifiConfig) {
+                    String s = wc.SSID;
+                    s = s.substring(1, s.length() - 1);
+                    res.add(s);
+                }
+        }
+        res = removeDuplicate(res);
+        Collections.sort(res);
+        return res;
+    }
+
+    private ArrayList<String> removeDuplicate(ArrayList<String> list) {
+        if (list == null)
+            return null;
+        HashSet<String> set = new HashSet<>(list);
+        return new ArrayList<>(set);
+    }
+
     public void saveWifiList(ArrayList<String> wifiList) {
         if (currentKey != null) {
+            Collections.sort(wifiList);
             SharedPreferencesHelper spHelper = SharedPreferencesHelper.getInstance();
             spHelper.setWifiList(currentKey, wifiList);
             currentKey = null;
+
+            loadEditText();
         } else {
             Toast.makeText(ctx, "Save fail!", Toast.LENGTH_SHORT).show();
         }
--- app/src/main/java/com/uet/beman/support/BM_WifiConnectionReceiver.java
+++ app/src/main/java/com/uet/beman/support/BM_WifiConnectionReceiver.java
@@ -8,31 +8,59 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
-import android.widget.Toast;
 
-public class BM_WifiConnectionReceiver extends BroadcastReceiver{
+import com.uet.beman.common.SharedPreferencesHelper;
+import com.uet.beman.util.Constant;
+
+import java.util.ArrayList;
+
+public class BM_WifiConnectionReceiver extends BroadcastReceiver {
+
+    private String getCurrentWifi(Context ctx) {
+        WifiManager wifiManager = (WifiManager) ctx.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        return wifiInfo.getSSID();
+    }
+
+    private boolean isWifiConnected(Context ctx) {
+        ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        Log.d("isWifiConnected", wifiInfo.toString());
+        NetworkInfo mobileInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        Log.d("isWifiConnected", mobileInfo.toString());
+
+        return (wifiInfo.isConnected() & !mobileInfo.isConnected());
+    }
+
+    private ArrayList<String> getSavedWifi(String key) {
+        SharedPreferencesHelper spHelper = SharedPreferencesHelper.getInstance();
+        return spHelper.getWifiList(key);
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("WifiReceiver", "Start onReceive");
+        Log.d("onReceive", "start onReceive");
 
-        ConnectivityManager connectManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        String s;
-        if (networkInfo.isConnected()){
-            WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-            if (wifiInfo != null){
-                s = wifiInfo.getSSID() + "/n" + wifiInfo.getMacAddress();
-            }else{
-                s = "No Wifi Information";
-            }
-        }else{
-            s = "No Connection";
+        if (!isWifiConnected(context)) {
+            Log.d("onReceive", "end onReceive 1");
+            return;
         }
 
-        Toast.makeText(context, s, Toast.LENGTH_LONG).show();
-        Log.d("WifiReceiver", s);
+        String currWifi = getCurrentWifi(context);
+        ArrayList<String> homeWifiList = getSavedWifi(Constant.HOME_WIFI_KEY);
+        ArrayList<String> workWifiList = getSavedWifi(Constant.WORK_WIFI_KEY);
+        ArrayList<String> girlWifiList = getSavedWifi(Constant.GIRL_HOUSE_WIFI_KEY);
 
-        Log.d("WifiReceiver", "End onReceive");
+        if (homeWifiList.contains(currWifi)) {
+            // At home
+        }
+        if (workWifiList.contains(currWifi)) {
+            // At work
+        }
+        if (girlWifiList.contains(currWifi)) {
+            // At girl's house
+        }
+
+        Log.d("onReceive", "end onReceive 2: " + currWifi);
     }
 }
