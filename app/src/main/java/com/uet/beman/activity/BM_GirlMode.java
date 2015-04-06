2a348aec0e3d43ce7e3ef1b3ecb3d7ca54f28f77
ntlam <ntlam94@gmail.com>
2015-04-07 00:13:07 +0700

    Fake Activity Layout + Link Web Page

--- app/src/main/java/com/uet/beman/activity/BM_GirlMode.java
+++ app/src/main/java/com/uet/beman/activity/BM_GirlMode.java
@@ -1,9 +1,12 @@
 package com.uet.beman.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.uet.beman.R;
 
@@ -37,5 +40,28 @@ public class BM_GirlMode extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void gotoWebPage(View view) {
+        String uri = "http://www.google.com/";
+        switch (view.getId()) {
+            case R.id.girl_mode_gift_layout_1:
+                uri = "http://www.dienhoatructuyen.vn/";
+                break;
+            case R.id.girl_mode_gift_layout_2:
+                uri = "https://chieuphimquocgia.com.vn/";
+                break;
+            case R.id.girl_mode_gift_layout_3:
+                uri = "http://trangsuctre.vn/";
+                break;
+            case R.id.girl_mode_gift_layout_4:
+                uri = "http://www.thuhuongbakery.com.vn";
+                break;
+            case R.id.girl_mode_gift_layout_5:
+                uri = "http://www.dangoai.com/";
+                break;
+        }
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        startActivity(browserIntent);
     }
 }
