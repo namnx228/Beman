71b90dcd6bb941b3f9529a03d358c2ea9e65e1c1
nam <namnx228@vnu.edu.vn>
2015-04-24 11:18:22 +0700

    messageHandler class

---
+++ app/src/main/java/com/uet/beman/operator/BM_MessageHandler.java
@@ -0,0 +1,27 @@
+package com.uet.beman.operator;
+
+/**
+ * Created by nam on 24/04/2015.
+ */
+public class BM_MessageHandler {
+    public void messageReadyToSend(int id, String alarmTime)
+    {
+        //set message to table_msg_time
+    }
+
+    public void send()
+    {}
+
+    public void preSend()
+    {}
+
+    public void postSend()
+    {}
+
+    public void sending()
+    {}
+
+    public void sendReply()
+    {}
+
+}
